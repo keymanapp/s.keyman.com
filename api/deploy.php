f7d2017d39bac3a2b55788bd9fb3340687416a21
Marc Durdin <marc@durdin.net>
2022-08-09 14:49:17 -0500

    fix: use github action to trigger deployment

--- api/deploy.php
+++ api/deploy.php
@@ -1,18 +1,16 @@
 <?php
 
+require_once 'webhooks.php';
+
 header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: max-age=0');
 
-if(!isset($_REQUEST['key']) || !isset($_SERVER['s_keyman_com_deploy_key'])) {
-  echo `{ "error": "Invalid key" }`;
-  exit(0);
+if(!isset($_SERVER['s_keyman_com_deploy_key'])) {
+  die('Incomplete setup');
 }
 
-$key = $_REQUEST['key'];
-if($key != $_SERVER['s_keyman_com_deploy_key']) {
-  echo `{ "error": "Invalid key" }`;
-  exit(0);
-}
+// If secret is not set, this will die
+$json = getWebhookData($_SERVER['s_keyman_com_deploy_key']);
 
 // This triggers the continuous webjob in App_Data/WebJobs/continuous/deploy
 file_put_contents('../.MUST_DEPLOY', "must deploy");
