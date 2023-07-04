e438d99b726f519e33c0f274c583199c57d55d2b
Darcy Wong <darcy_wong@sil.org>
2023-07-04 10:18:37 -0500

    refactor: Handle error if no versions found

--- api/kmwversion.sh
+++ api/kmwversion.sh
@@ -5,30 +5,42 @@ set -eu
 
 . ./jq.inc.sh
 
-# Get a directory listing of KMW versions
-function _getKMWVersions () {
-  cd ../kmw/engine
-  local arr=( */ );       # This creates an array of the full paths to all subdirs
-  arr=("${arr[@]%/}")     # This removes the trailing slash on each item
-  arr=("${arr[@]##*/}")   # This removes the path prefix, leaving just the dir names
-  cd ../../api
-  echo ${arr[*]}
-}
-
-
 # Sort the versions in descending order
 # https://stackoverflow.com/questions/65315720/how-to-sort-release-version-string-in-descending-order-with-bash
 function _sortVersions () {
-  sorted=( $(echo "$@" | sed 's/ /\n/g' | sort -t. -k1,1rn -k2,2rn -k3,3rn) )
+  local sorted=( $(echo "$@" | sed 's/ /\n/g' | sort -t. -k1,1rn -k2,2rn -k3,3rn) )
   echo "${sorted[*]}"
 }
 
-declare -a v
-v=$(_getKMWVersions)
-#echo "v is ${v[*]}"
+# Get a directory listing of KMW versions sorted in descending order
+function _search () {
+  cd ../kmw/engine
+  local arr=( */ );       # This creates an array of the full paths to all subdirs
+  if [ "${arr[0]}" != "*/" ]; then
+    arr=("${arr[@]%/}")     # This removes the trailing slash on each item
+    arr=("${arr[@]##*/}")   # This removes the path prefix, leaving just the dir names
 
-sortedVersions=$(_sortVersions "${v[@]}")
-#echo "sorted is ${sortedVersions[*]}"
+    #echo "before sort, arr is ${arr[@]}"
+    arr=$(_sortVersions "${arr[@]}")
+    echo ${arr[*]}
+  fi  
 
-# Create JSON file and pretty-print
-$JQ -nc '{versions: $ARGS.positional}' --args ${sortedVersions[@]} | $JQ --indent 4 > ../metadata/kmwversions.json
+  cd ../../api
+}
+
+# Write result to JSON file
+function _emit () {
+  if [ ! -z "$result" ]; then
+    $JQ -nc '{versions: $ARGS.positional}' --args ${result[@]} | \
+      $JQ --indent 4 > ../metadata/kmwversions.json 
+  else
+    $JQ -nc '{error: "No releases found"}' | \
+      $JQ --indent 4 > ../metadata/kmwversions.json    
+  fi
+}
+
+declare -a result
+result=$(_search)
+#echo "result is ${result[*]}"
+
+_emit
