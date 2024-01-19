7c23b4a18d1b13a840f785f8c561469bf86d94a8
Darcy Wong <darcy_wong@sil.org>
2024-01-19 09:25:03 +0700

    fix: Apply review comments

--- _control/info.php
+++ _control/info.php
@@ -1,5 +1,19 @@
 <?php
-  require_once __DIR__ . '/../tools/autoload.php';
-  use Keyman\Site\Common\KeymanHosts;
+  // Determine tier - copied from KeymanHosts.php
+  $env = getenv();
+  const TIER_DEVELOPMENT = "TIER_DEVELOPMENT";
+  const TIER_STAGING = "TIER_STAGING";
+  const TIER_PRODUCTION = "TIER_PRODUCTION";
+  const TIER_TEST = "TIER_TEST";
 
-  echo "<p>TIER: " . KeymanHosts::Instance()->Tier() . "</p>";
+  if(isset($env['KEYMANHOSTS_TIER']) && in_array($env['KEYMANHOSTS_TIER'],
+      [TIER_DEVELOPMENT, TIER_STAGING,
+       TIER_PRODUCTION, TIER_TEST])) {
+    $tier = $env['KEYMANHOSTS_TIER'];
+  } else if(file_exists(__DIR__ . '/../tier.txt')) {
+    $tier = trim(file_get_contents(__DIR__ . '/../tier.txt'));
+  } else {
+    $tier = TIER_DEVELOPMENT;
+  }
+
+  echo "<p>TIER: " . $tier . "</p>";
